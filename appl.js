011471d882952e9f2bc2e0735c06ebc9b039ba28
taufik0910 <taufik.cradle@gmail.com>
2018-10-12 15:10:46 +0700

    coba lagi

--- appl.js
+++ appl.js
@@ -2,7 +2,7 @@ const Telegram =require('node-telegram-bot-api')
 const dbot=require('dbot-js')
 
 const token= '665897823:AAEzsfG8Rx8l4TKFXcOpT1FtliKMuPil2fA'
-const taufik0910_bot = new TelegramBot (token,{polling: true});
+const taufik0910_bot = new TelegramBot (token, { polling: true});
 
 taufik0910_bot.on('message', function (msg) {
     var charID = msg.chat.id;
