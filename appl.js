b9af903efe8578726fa2f46fb4b44bde4f95152e
taufik0910 <taufik.cradle@gmail.com>
2018-10-12 15:36:18 +0700

    revisi appl terbaru

--- appl.js
+++ appl.js
@@ -1,18 +1,16 @@
-const Telegram =require('node-telegram-bot-api')
-const dbot=require('dbot-js')
+Telegram = require('node-telegram-bot-api')
+const dbot = require('dbot-js');
+const token = '665897823:AAEzsfG8Rx8l4TKFXcOpT1FtliKMuPil2fA';
+const bot = new Telegram(token, { polling: true });
 
-const token= '665897823:AAEzsfG8Rx8l4TKFXcOpT1FtliKMuPil2fA'
-const taufik0910_bot = new TelegramBot (token, { polling: true});
-
-taufik0910_bot.on('message', function (msg) {
-    var charID = msg.chat.id;
-    var message = msg.text.toString()
-    dbot.get_response(message,function (err, result) {
-        if (!err) {
-            taufik0910_bot.sendMessage(charID, result)
-            
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    var message = msg.text.toString();
+    dbot.get_response(message, function(err, result){
+        if(!err) {
+            bot.sendMessage(chatId, result)
+        }else{
+            console.log(err)
         }
-        
-    })
-    
-})
+    });
+});
